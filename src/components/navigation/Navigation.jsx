1b2b8fb2ca4cd7e9dab65398d94b1c7faa1cc307
Malik-Haziq <malikhaziq153@gmail.com>
2023-12-01 03:59:31 +0500

    images paths

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,10 +1,14 @@
 import '../../css/index.css'
-import logo from '../../../assets/icons/logo.svg'
-import mail from '../../../assets/icons/mail.svg'
-import phone from '../../../assets/icons/phone.svg'
-import downArrow from '../../../assets/icons/chivron-down.svg'
-import Button from '../button/button'
+import logo from '@icons/logo.svg'
+import mail from '@icons/mail.svg'
+import phone from '@icons/phone.svg'
+import downArrow from '@icons/chivron-down.svg'
+import menu from '@icons/menu.svg'
+import cross from '@icons/cross.svg'
+import Button from '@components/button/button'
+import { useState } from 'react'
 export default function Navigation() {
+  const [showNav, setShowNav] = useState(true)
   return (
     <nav>
       <div className='bg-secondary padding flex gap-8 justify-end text-para'>
@@ -12,16 +16,18 @@ export default function Navigation() {
         <a className='flex gap-1 items-center justify-center'><img src={phone} alt="phone icon" className='w-4'/>+923074485229</a>
         <a className='flex gap-1 items-center justify-center'><img src={phone} alt="phone icon" className='w-4'/>+923054388079</a>
       </div>
-      <div className='padding flex justify-between items-center'>
+      <div className='padding flex justify-between items-center relative'>
         <img src={logo} alt="cwn logo" />
+        <img src={menu} alt="menu" className='w-8 absolute right-0'/>
+        <img src={cross} alt="Close" className='w-8 absolute right-0'/>
         <div>
-          <ul className='flex items-center justify-center gap-10'>
-            <li><a href="#" className='flex gap-1 items-center justify-center'><span>About</span><img src={downArrow} alt="down arrow" className='w-5'/></a></li>
-            <li><a href="#" className='flex gap-1 items-center justify-center'><span>Services</span><img src={downArrow} alt="down arrow" className='w-5'/></a></li>
-            <li><a href="#" className='flex gap-1 items-center justify-center'><span>Solutions</span><img src={downArrow} alt="down arrow" className='w-5'/></a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Blogs</a></li>
-            <li><a href="#">Company</a></li>
+          <ul className='absolute left-11 top-full flex flex-col gap-10'>
+            <li><a href="#" className='flex gap-2 items-center justify-center hover:text-main-shade duration-300'><span>About</span><img src={downArrow} alt="down arrow" className='w-4'/></a></li>
+            <li><a href="#" className='flex gap-2 items-center justify-center hover:text-main-shade duration-300'><span>Services</span><img src={downArrow} alt="down arrow" className='w-4'/></a></li>
+            <li><a href="#" className='flex gap-2 items-center justify-center hover:text-main-shade duration-300'><span>Solutions</span><img src={downArrow} alt="down arrow" className='w-4'/></a></li>
+            <li><a href="#" className='hover:text-main-shade duration-300'>Portfolio</a></li>
+            <li><a href="#" className='hover:text-main-shade duration-300'>Blogs</a></li>
+            <li><a href="#" className='hover:text-main-shade duration-300'>Company</a></li>
           </ul>
         </div>
         <Button text={'Get in Touch'} href={''}/>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@styles': '/src/css',
+      '@images': '/src/assets/images',
+      '@icons': '/src/assets/icons'
+    },
+  },
+});
