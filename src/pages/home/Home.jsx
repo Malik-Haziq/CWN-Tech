8a639d582584a9e5f84c202e76379b05c286aea1
Malik-Haziq <malikhaziq153@gmail.com>
2023-12-21 04:43:52 +0500

    who we are is done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -178,16 +178,16 @@ export function Home() {
       </section>
 
       {/* /////////////////////// Who we are */}
-      <section className="section mb-32 flex">
-        <div>
-          <h2 className="h2">Who we are</h2>
-          <p>CWN Technologies is a full-cycle app & software development company which covers specific client business needs with the help of the best possible technology solutions.</p>
-          <p>Since 2018, we have been inventing digital solutions, helping startups and SMBs come out on top in their markets.</p>
+      <section className="section mb-32 flex flex-col lg:flex-row justify-between items-center">
+        <div className="basis-1/2 text-lg md:text-xl">
+          <h2 className="h2 mb-8 sm:mb-16">Who we are</h2>
+          <p className="text-sub-para mb-4">CWN Technologies is a full-cycle app & software development company which covers specific client business needs with the help of the best possible technology solutions.</p>
+          <p className="text-sub-para">Since 2018, we have been inventing digital solutions, helping startups and SMBs come out on top in their markets.</p>
         </div>
         <img
             src={challenge}
             alt="Problem solving"
-            className="mx-auto w-[400px] md:hidden xlg:inline-block xlg:w-unset "
+            className="mx-auto w-[400px] xlg:w-unset "
           />
       </section>
     </main>
