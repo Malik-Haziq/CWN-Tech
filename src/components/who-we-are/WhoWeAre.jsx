54c25afe206027a2056becb6466c1f3a5e940e4d
Malik-Haziq <malikhaziq153@gmail.com>
2024-01-05 05:01:01 +0500

    who we are component created

---
+++ src/components/who-we-are/WhoWeAre.jsx
@@ -0,0 +1,26 @@
+import challenge from "@images/challenge/Problem solving-amico.svg";
+
+
+export default function WhoWeAre() {
+  return (
+    <section className="section mb-32 flex flex-col lg:flex-row justify-between items-center">
+      <div className="basis-1/2 text-lg md:text-xl">
+        <h2 className="h2 mb-8 sm:mb-16">Who we are</h2>
+        <p className="text-sub-para mb-4">
+          CWN Technologies is a full-cycle app & software development company
+          which covers specific client business needs with the help of the best
+          possible technology solutions.
+        </p>
+        <p className="text-sub-para">
+          Since 2018, we have been inventing digital solutions, helping startups
+          and SMBs come out on top in their markets.
+        </p>
+      </div>
+      <img
+        src={challenge}
+        alt="Problem solving"
+        className="mx-auto w-[400px] xlg:w-unset "
+      />
+    </section>
+  );
+}
