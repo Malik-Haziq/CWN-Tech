01f4340cb153b1994cb4ae908c8d6d85df56affe
Malik-Haziq <malikhaziq153@gmail.com>
2024-01-08 04:48:33 +0500

    added bookCall component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,6 +14,7 @@ import Button from "@components/button/button";
 import Contact from "@components/contact/contact";
 import Footer from "@components/footer/Footer";
 import WhoAeAre from "@components/who-we-are/WhoWeAre";
+import BookCall from "@components/book-call/BookCall";
 
 import { useState } from "react";
 
@@ -196,23 +197,7 @@ export function Home() {
       </section>
 
       {/* /////////////////////////  Book a call */}
-      <section className="section bg-main-shade mb-32  ">
-        <div className="flex flex-col gap-3 items-center py-14">
-          <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl text-center text-white font-poppins max-w-[950px]">
-            Ready to Discuss Your Project? or Let's Discuss Your Project
-          </h2>
-          <p className="text-center text-light leading-7 mb-4 text-lg max-w-[900px]">
-            We're excited to help you achieve your goals and transform them into
-            a top-notch digital solution. Claim your free consultation now!
-          </p>
-          <Button text="Book a Call" styles="bg-transparent text-black mb-5" />
-          <p className="text-light text-center">
-            Or, drop your details in the
-            <a href="#contact" className="text-[#ffc16b] font-semibold"> form </a>
-            below to share your requirements.
-          </p>
-        </div>
-      </section>
+      <BookCall/>
 
       {/* /////////////////////// Who we are */}
       <WhoAeAre />
